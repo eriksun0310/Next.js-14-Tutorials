dc3a97ecfeb7f696d9fde1ca597056ca1ca64a7f
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-07-11 17:29:11 +0800

    42 end

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,17 +1,26 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
+  // 取得 http header
   const requestHeaders = new Headers(request.headers);
   const headerList = headers();
 
-  console.log(requestHeaders.get("Authorization"));
+  // 設定cookie
+  cookies().set("resultPerPage", "20");
+  //取得cookie
+  const theme = request.cookies.get("theme");
 
-  console.log(headerList.get("Authorization"));
+  //   console.log(requestHeaders.get("Authorization"));
+  //   console.log(headerList.get("Authorization"));
 
+  console.log("theme", theme);
+  //取得cookie
+  console.log(cookies().get("resultPerPage"));
   return new Response("<h1>Profile Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark", // 設定cookie
     },
   });
 }
